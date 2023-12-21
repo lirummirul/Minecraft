9083af3b0114fa62810184078bc2220c359638a1
lirummirul <89711820+lirummirul@users.noreply.github.com>
2023-12-22 01:56:01 +0300

    edits

--- src/main/java/com/example/examplemod/block/TileBlock.java
+++ src/main/java/com/example/examplemod/block/TileBlock.java
@@ -2,6 +2,7 @@ package com.example.examplemod.block;
 
 import com.example.examplemod.entity.TileEntity;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -13,11 +14,19 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class TileBlock extends Block implements EntityBlock {
     public TileBlock(BlockBehaviour.Properties properties) {
         super(properties);
     }
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+        return Shapes.box(0.125D, 0.125D, 0.125D, 0.875D, 0.875D, 0.875D);
+    }
+
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
         ItemStack heldItem = player.getItemInHand(hand);
         BlockEntity blockEntity = level.getBlockEntity(pos);
--- src/main/java/com/example/examplemod/render/TileRender.java
+++ src/main/java/com/example/examplemod/render/TileRender.java
@@ -2,6 +2,7 @@ package com.example.examplemod.render;
 
 import com.example.examplemod.entity.TileEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -9,6 +10,7 @@ import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -30,13 +32,16 @@ public class TileRender implements BlockEntityRenderer<TileEntity> {
     }
     public static void renderItemFixed(TileEntity tileEntity, Level level, PoseStack poseStack, MultiBufferSource source, ItemStack itemStack, double transX, double transY, double transZ, int combinedLight, int combinedOverlay) {
         if (!itemStack.isEmpty()) {
+            float angle = (float) (System.currentTimeMillis() / 10 % 360);
+
             poseStack.pushPose();
             poseStack.translate(transX, transY, transZ);
+            poseStack.mulPose(Vector3f.YP.rotationDegrees(angle)); // Вращение вокруг оси Y
             poseStack.scale(0.5F, 0.5F, 0.5F);
             ItemRenderer renderer = Minecraft.getInstance().getItemRenderer();
 //            System.out.println("combinedLight : " + combinedLight + " combinedOverlay : " + combinedOverlay);
             renderer.renderStatic((LivingEntity)null, itemStack, ItemTransforms.TransformType.GUI,
-                    true, poseStack, source, level, combinedLight, combinedOverlay, getLightLevel(level, tileEntity.getBlockPos()));
+                    true, poseStack, source, level, combinedLight, OverlayTexture.NO_OVERLAY, getLightLevel(level, tileEntity.getBlockPos()));
             poseStack.popPose();
         }
     }
